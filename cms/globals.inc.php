38cc515e4482429f9df4b3d2fe63bdd00e5314a6
Steven Bower <steven@turnwheel.com>
2011-11-02 21:00:45 -0700

    Fix SQL in global

--- cms/globals.inc.php
+++ cms/globals.inc.php
@@ -25,10 +25,12 @@ require LPATH.'utility.inc.php';
 require LPATH.'security.inc.php';
 
 // Load SQL if db is enabled
-if ($cfg['db_enable']) require LPATH.'sql.inc.php';
+if ($cfg['db_enable']) {
+	require LPATH.'sql.inc.php';
 
-sql_connect($cfg['db_host'],$cfg['db_user'],$cfg['db_pass'],$cfg['db_name']); // Connect to SQL Server
-unset($cfg['db_user'],$cfg['db_pass']); // Security Measure
+	sql_connect($cfg['db_host'],$cfg['db_user'],$cfg['db_pass'],$cfg['db_name']); // Connect to SQL Server
+	unset($cfg['db_user'],$cfg['db_pass']); // Security Measure
+}
 
 // Encrypt admin pass so it's not stored in plain text
 if (isset($cfg['admin']['pass'])) {
