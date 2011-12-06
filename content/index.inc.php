ca7996913db1502ae3240abb1709ad3c333ba91b
Steven Bower <steven@turnwheel.com>
2011-12-06 12:51:43 -0800

    Cut code from a different website

--- content/index.inc.php
+++ content/index.inc.php
@@ -6,30 +6,6 @@ ob_start();
 
 <h3>Use this page as your main home!</h3>
 
-<div style="width:100%;height:200px;margin:10px 0;">
-	<div class="border fleft" style="width:180px;height:180px;margin-top:10px;">
-		<a href="/contact/">
-			<img src="/images/map.png" alt="Map" style="width:180px;height:180px;" />
-		</a>
-	</div>
-	<div class="fright" style="width:390px;height:200px;margin-top:10px;">
-		<p>
-			We are located in Santa Rosa's Historice Railroad
-			Square District in downtown Santa Rosa.
-			We are open daily:
-		</p>
-		<p>
-			Monday thru Saturday, 10am &mdash; 6pm<br />
-			Sunday, 11am &mdash; 5pm
-		</p>
-		<p>
-			101 Third Street<br />
-			Santa Rosa, CA 95401
-		</p>
-	</div>
-</div>
-<div class="clear"></div>
-
 <hr />
 
 <div>
