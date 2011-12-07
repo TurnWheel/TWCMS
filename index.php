4044236ee19359523a4771d6c2d4691ad64b8851
Steven Bower <steven@turnwheel.com>
2011-12-07 01:07:21 -0800

    Clean up navigation

--- index.php
+++ index.php
@@ -65,7 +65,6 @@ foreach ($cfg['t_js'] AS $file) {
 				<li><a href="/">Home</a></li>
 				<li><a href="/about">About Us</a></li>
 				<li><a href="/something">Something Cool</a></li>
-				<li><a href="/lame">Lame</a></li>
 				<li><a href="/contact">Contact Us</a></li>
 			</ul>
 		</div>
