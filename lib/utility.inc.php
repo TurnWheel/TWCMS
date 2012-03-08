3c1e28d479d791a09b7904f5e00d17b8d4db210b
Steven Bower <steven@turnwheel.com>
2012-03-08 01:39:14 -0800

    Add httpCodes and states utility arrays to utility lib

--- lib/utility.inc.php
+++ lib/utility.inc.php
@@ -940,4 +940,90 @@ function rfc3696_strip_comments($comment, $email, $replace='') {
 	}
 }
 
+/*
+ * <TWCMS>
+ *
+ * Utility Arrays
+ *
+ * Useful arrays for common information
+ * Kept here so not to clog up config file
+ */
+
+// HTTP Status codes and their proper messages
+$cfg['httpCodes'] = array(
+		// [Informational 1xx]
+		100 => '100 Continue',
+		101 => '101 Switching Protocols',
+
+		// [Successful 2xx]
+		200 => '200 OK',
+		201 => '201 Created',
+		202 => '202 Accepted',
+		203 => '203 Non-Authoritative Information',
+		204 => '204 No Content',
+		205 => '205 Reset Content',
+		206 => '206 Partial Content',
+
+		// [Redirection 3xx]
+		300 => '300 Multiple Choices',
+		301 => '301 Moved Permanently',
+		302 => '302 Found',
+		303 => '303 See Other',
+		304 => '304 Not Modified',
+		305 => '305 Use Proxy',
+		306 => '306 (Unused)',
+		307 => '307 Temporary Redirect',
+
+		// [Client Error 4xx]
+		400 => '400 Bad Request',
+		401 => '401 Unauthorized',
+		402 => '402 Payment Requred',
+		403 => '403 Forbidden',
+		404 => '404 Not Found',
+		405 => '405 Method Not Allowed',
+		406 => '406 Not Acceptable',
+		407 => '407 Proxy Authentication Required',
+		408 => '408 Request Timeout',
+		409 => '409 Conflict',
+		410 => '410 Gone',
+		411 => '411 Length Required',
+		412 => '412 Precondition Failed',
+		413 => '413 Request Entity Too Large',
+		414 => '414 Request-URI Too Long',
+		415 => '415 Unsupported Media Type',
+		416 => '416 Requested Range Not Satisfiable',
+		417 => '417 Expectation Failed',
+
+		// [Server Error 5xx]
+		500 => '500 Internal Server Error',
+		501 => '501 Not Implemented',
+		502 => '502 Bad Gateway',
+		503 => '503 Service Unavailable',
+		504 => '504 Gateway Timeout',
+		505 => '505 HTTP Version Not Supported'
+);
+
+
+// Array of US States
+// Used for form fields and validation
+// and for other display purposes
+$cfg['states'] = array(
+	'AL' => 'Alabama','AK' => 'Alaska','AZ' => 'Arizona',
+	'AR' => 'Arkansas','CA' => 'California','CO' => 'Colorado',
+	'CT' => 'Connecticut','DE' => 'Delaware','DC' => 'District Of Columbia',
+	'FL' => 'Florida','GA' => 'Georgia','HI' => 'Hawaii','ID' => 'Idaho',
+	'IL' => 'Illinois','IN' => 'Indiana','IA' => 'Iowa','KS' => 'Kansas',
+	'KY' => 'Kentucky','LA' => 'Louisiana','ME' => 'Maine','MD' => 'Maryland',
+	'MA' => 'Massachusetts','MI' => 'Michigan','MN' => 'Minnesota',
+	'MS' => 'Mississippi','MO' => 'Missouri','MT' => 'Montana',
+	'NE' => 'Nebraska','NV' => 'Nevada','NH' => 'New Hampshire',
+	'NJ' => 'New Jersey','NM' => 'New Mexico','NY' => 'New York',
+	'NC' => 'North Carolina','ND' => 'North Dakota','OH' => 'Ohio',
+	'OK' => 'Oklahoma','OR' => 'Oregon','PA' => 'Pennsylvania',
+	'RI' => 'Rhode Island','SC' => 'South Carolina','SD' => 'South Dakota',
+	'TN' => 'Tennessee','TX' => 'Texas','UT' => 'Utah','VT' => 'Vermont',
+	'VA' => 'Virginia','WA' => 'Washington','WV' => 'West Virginia',
+	'WI' => 'Wisconsin','WY' => 'Wyoming'
+);
+
 // EOF
