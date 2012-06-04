fd1234ab95c9264a20f3a0afd3e78a5d91980c41
Steven Bower <steven@turnwheel.com>
2012-06-03 17:37:30 -0700

    [Errors Mod] Move static config val to error.cfg

--- mods/error/error.inc.php
+++ mods/error/error.inc.php
@@ -137,25 +137,4 @@ function error_parse_val($value) {
 	return '<strong>Unable to determine variable type.</strong>';
 }
 
-/*
- * Static config values
- */
-
-// Array of error values and their string equivolent
-$cfg['error_vals'] = array(
-	'1'		=> 'E_ERROR',
-	'2'		=> 'E_WARNING',
-	'4'		=> 'E_PARSE',
-	'8'		=> 'E_NOTICE',
-	'16'	=> 'E_CORE_ERROR',
-	'32'	=> 'E_CORE_WARNING',
-	'64'	=> 'E_COMPILE_ERROR',
-	'128'	=> 'E_COMPILE_WARNING',
-	'256'	=> 'E_USER_ERROR',
-	'512'	=> 'E_USER_WARNING',
-	'1024'	=> 'E_USER_NOTICE',
-	'2047'	=> 'E_ALL',
-	'2048'	=> 'E_STRICT'
-);
-
 // EOF
