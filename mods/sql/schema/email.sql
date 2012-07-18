da18f03face2f529a168188e9e29e0a94af9d6a9
Steven Bower <steven@turnwheel.com>
2012-07-18 15:27:42 -0700

    Fix error in email sql schema

--- mods/sql/schema/email.sql
+++ mods/sql/schema/email.sql
@@ -1,7 +1,7 @@
 -- Tracks all emails sent through tw_sendmail
 CREATE TABLE email (
 	emailid int(10) auto_increment NOT NULL,
-	to TINYTEXT NOT NULL, -- List of email addresses sent to (comma separated)
+	`to` TINYTEXT NOT NULL, -- List of email addresses sent to (comma separated)
 	subject varchar(255) NOT NULL, -- Subject of remail
 	body TEXT NOT NULL, -- Full body of email
 	headers TINYTEXT NOT NULL, -- Additional headers sent in email
